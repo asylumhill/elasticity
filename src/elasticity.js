5960d92b6a841fecd8758c60a7a8157b1ab9a3e0
Thomas Haslam <Tom@Toms-Mac.local>
2015-11-29 00:01:34 -0500

    initial check-in

---
+++ src/elasticity.js
@@ -0,0 +1,140 @@
+/*!
+* Tally Input Control v1.0 (jQuery Plugin)
+* (c) Asylum Hill Software, LLC
+* License: MIT (http://www.opensource.org/licenses/mit-license.php)
+*/
+
+(function ($) {
+  'use strict';
+    
+  $.fn.elasticity = function (options) {
+    this.name = "ElasticityControlV1";
+    this.columnsDomIdPrefix = "smartLayoutElasticityColumn";
+    this._firstStopWidth = true;
+    
+    this._init = function () { 
+      
+      // when window resizes we want to re-evaluate cards
+      $(window).bind("resize." + this.id, {
+        instance: this
+      }, this._layoutCards);
+            
+      // make sure container grows with content
+      this.css({overflow: "hidden"});
+              
+      var e = {target: $(window),
+      data: {instance: this}};
+      this._layoutCards(e);
+    }
+          
+    this.getwidth = function () {
+      return this._width;
+    };
+    
+    this._calculateBoardWidth = function (windowWidth) {
+      var boardWidth;
+      this._numberOfColumns = Math.floor((windowWidth - settings.leftOffset) / (settings.blockWidth + settings.triggerOffset));
+      if(this._numberOfColumns <= settings.minColumns) {
+        boardWidth = settings.minColumns * settings.blockWidth;
+      } else {
+        boardWidth = this._numberOfColumns * settings.blockWidth;
+      }
+      return boardWidth;
+    };
+        
+    this._setBoardWidth = function (windowWidth) {
+      var instance = this;
+      var newBoardWidth = this._calculateBoardWidth.call(this, windowWidth);
+      
+      // evaluate if we should stop setting the width for anything
+      if(newBoardWidth == this._width) {
+        return false;
+      }
+      
+      instance._width = newBoardWidth;
+      $.each(settings.linkWithDomIds, function( index, value ) {
+        $('#' + value).css("width", instance._width.toString() + "px");
+      });
+  
+      return true;
+    };
+        
+    this._buildBoardColumns = function () {
+      var instance = this;
+      for(var col = 1; col <= this._numberOfColumns; col++) {
+        if($("#" + this.columnsDomIdPrefix + col.toString()).length <= 0) {
+          var column = $("<div id=\"" + instance.columnsDomIdPrefix + col.toString() + "\"></div>").css({
+              display: "inline-block",
+              position: "relative",
+              float: "left",
+              top: "0px",
+          });
+          this.append(column);
+        }
+      }
+    };
+        
+    this._setCardPositions = function () {
+      var instance = this;
+      //var domCards = $("#" + this.id +  " [data-card='true']");
+      var domCards = this.find("[data-card='true']");
+      if(!domCards) return;
+      
+      var colCount = 0;
+      domCards.each(function(index, element){
+          colCount++;
+          var col = instance.find("#" + instance.columnsDomIdPrefix + colCount);
+          $( this ).appendTo(col);
+          if(colCount == instance._numberOfColumns) colCount = 0;
+      });
+    };
+        
+    this.clear = function () {
+      var domCards = this.find("[data-card='true']");
+      domCards.each(function (index) {
+          $(this).remove();
+      });
+    };
+        
+    this._layoutCards = function (e) {
+      var windowWidth = $(e.target).width();
+      var instance = e.data.instance;
+      
+      // evaluate if we should stop elastic rending because of a width threshold
+      if(windowWidth <= settings.stopWidth && instance._firstStopWidth == false) return;
+      else if(windowWidth <= settings.stopWidth)
+        instance._firstStopWidth = false;
+      else
+        instance._firstStopWidth = true;
+        
+      if(!instance._setBoardWidth.call(instance, windowWidth)) return;
+      instance._buildBoardColumns.apply(instance);
+      instance._setCardPositions.apply(instance);
+    };
+    
+    // set default options
+    var settings = $.extend({
+        blockWidth: 0,
+        leftOffset: 0,
+        triggerOffset: 0,
+        stopWidth: 0,
+        minColumns: 0,
+        linkWithDomIds: []
+    }, options );
+    
+    // wire up control
+    this._init();
+  
+    // set default style
+    return this.css({
+        blockWidth: 385,
+        leftOffset: 0,
+        triggerOffset: 0,
+        stopWidth: 767,
+        minColumns: 1,
+        linkWithDomIds: []
+    });
+ 
+  };
+ 
+}( jQuery ));
